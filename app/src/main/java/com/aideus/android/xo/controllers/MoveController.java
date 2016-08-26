50b6d18adad257d01f4276e9ee944b1f34cc0467
begimov <begimov@gmail.com>
2016-08-26 17:14:11 +0300

    MoveController created

---
+++ app/src/main/java/com/aideus/android/xo/controllers/MoveController.java
@@ -0,0 +1,17 @@
+package com.aideus.android.xo.controllers;
+
+import com.aideus.android.xo.model.Field;
+import com.aideus.android.xo.model.Figure;
+import com.aideus.android.xo.model.Point;
+
+/**
+ * Created by Galymzhan Begimov on 26.08.2016.
+ */
+
+public class MoveController {
+
+    public void applyFigure(Field field, Figure figure, Point point) {
+
+    }
+
+}
