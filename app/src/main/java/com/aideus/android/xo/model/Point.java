f4a0ddea5abb5414e4ecd511c1ba77c5faca01d2
begimov <begimov@gmail.com>
2016-08-26 16:13:31 +0300

    Point created

---
+++ app/src/main/java/com/aideus/android/xo/model/Point.java
@@ -0,0 +1,25 @@
+package com.aideus.android.xo.model;
+
+/**
+ * Created by Galymzhan Begimov on 26.08.2016.
+ */
+
+public class Point {
+
+    private final int x;
+
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
