41713d9fa345740ca3217962c01184918e9b4098
begimov <begimov@gmail.com>
2016-08-26 20:14:44 +0300

    MoveController: refactoring

--- app/src/main/java/com/aideus/android/xo/controllers/MoveController.java
+++ app/src/main/java/com/aideus/android/xo/controllers/MoveController.java
@@ -3,6 +3,8 @@ package com.aideus.android.xo.controllers;
 import com.aideus.android.xo.model.Field;
 import com.aideus.android.xo.model.Figure;
 import com.aideus.android.xo.model.Point;
+import com.aideus.android.xo.model.exceptions.AlreadyOccupiedException;
+import com.aideus.android.xo.model.exceptions.InvalidPointException;
 
 /**
  * Created by Galymzhan Begimov on 26.08.2016.
@@ -10,7 +12,16 @@ import com.aideus.android.xo.model.Point;
 
 public class MoveController {
 
-    public void applyFigure(Field field, Figure figure, Point point) {
+    public void applyFigure(final Field field,
+                            final Figure figure,
+                            final Point point)
+            throws InvalidPointException, AlreadyOccupiedException {
+
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+
+        field.setFigure(point, figure);
 
     }
 
