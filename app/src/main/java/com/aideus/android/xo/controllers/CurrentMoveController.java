0c1c04a329ba8b937b68dbda0d97940ff35c9695
begimov <begimov@gmail.com>
2016-08-31 15:12:51 +0300

    CurrentMoveController: created

---
+++ app/src/main/java/com/aideus/android/xo/controllers/CurrentMoveController.java
@@ -0,0 +1,34 @@
+package com.aideus.android.xo.controllers;
+
+import com.aideus.android.xo.model.Field;
+import com.aideus.android.xo.model.Figure;
+import com.aideus.android.xo.model.Point;
+import com.aideus.android.xo.model.exceptions.InvalidPointException;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Field field) {
+
+        int countFigure = 0;
+
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int j = 0; j < field.getSize(); j++) {
+                try {
+                    if (field.getFigure(new Point(i,j)) != null) {
+                        countFigure++;
+                    }
+                } catch (InvalidPointException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (countFigure == field.getSize() * field.getSize()) {
+            return null;
+        } else if (countFigure % 2 == 0){
+            return Figure.X;
+        } else {
+            return Figure.O;
+        }
+    }
+
+}
