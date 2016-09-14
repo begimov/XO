2815c0bdc05d9fb8fb1f23ca5557e2af0924b7c5
begimov <begimov@gmail.com>
2016-09-14 11:38:28 +0300

    Game iterable by <Player>, custom Iterator

--- app/src/main/java/com/aideus/android/xo/data/model/Game.java
+++ app/src/main/java/com/aideus/android/xo/data/model/Game.java
@@ -1,8 +1,6 @@
 package com.aideus.android.xo.data.model;
 
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 
 public class Game implements Iterable<Player> {
 
@@ -34,7 +32,26 @@ public class Game implements Iterable<Player> {
 
     @Override
     public Iterator<Player> iterator() {
-        final List<Player> playersList = Arrays.asList(players);
-        return playersList.iterator();
+        return new PlayerIterator();
+    }
+
+    private class PlayerIterator implements Iterator<Player> {
+
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return Game.this.players.length > index;
+        }
+
+        @Override
+        public Player next() {
+            return Game.this.players[index++];
+        }
+
+        @Override
+        public void remove() {
+
+        }
     }
 }
